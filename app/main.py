a0b51bf1dcb79f96d86a6d2424625b44ae1199f2
Neon Kiwi <ryjen7@gmail.com>
2023-07-07 21:12:38 -0400

    add experimental endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route("/hi/<person>")
+def hi(person):
+    return "Hi " + person
+
 @app.route("/")
 def hello():
     return "Hello from Python!"
