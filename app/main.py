b14720523f203639d18b8a608a3d8ceab28d8cb6
Leif Jensen <ljensen@quantixcommodities.com>
2023-07-08 22:03:57 -0400

    added a counting endpoint with a for statement

--- app/main.py
+++ app/main.py
@@ -20,6 +20,14 @@ Enough for now!
 </html>
 """
 
+@app.route("/count/<int:n>")
+def count(n):
+    ret = f"<html><body><h1>Counting to {n}</h1>\n<ul>"
+    for j in range(1, n + 1):
+        ret += f"<li>{j}</li>\n"
+    ret += "</ul>\n</body></html>"
+    return ret, 200
+
 @app.route("/")
 def hello():
     return "Hello from Python!"
