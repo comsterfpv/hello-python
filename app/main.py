7a5402222da44e8a3ac252483b58b5ff11f1117a
Neon Kiwi <ryjen7@gmail.com>
2023-07-13 21:45:44 -0400

    added factor and random endpoints

--- app/main.py
+++ app/main.py
@@ -28,6 +28,25 @@ def count(n):
     ret += "</ul>\n</body></html>"
     return ret, 200
 
+@app.route("/factor/<int:n>")
+def factor(n):
+    ret = f"<html><body><h1>Factors of {n}</h1>\n<ul>"
+    for j in range(1, n + 1):
+        if (n/j)%1 == 0:
+            ret += f"<li>{int(n/j)}</li>\n"
+    ret += "</ul>\n</body></html>"
+    return ret, 200
+
+@app.route("/random/<int:n>")
+def random(n):
+    ret = f"<html><body><h1>5 random numbers from 0 to {n}</h1>\n<ul>"
+    for j in range(5):
+        import random
+        rand = random.randint(0,n)
+        ret += f"<li>{rand}</li>\n"
+    ret += "</ul>\n</body></html>"
+    return ret, 200
+
 @app.route("/")
 def hello():
     return "Hello from Python!"
